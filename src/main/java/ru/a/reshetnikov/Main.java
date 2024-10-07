892ee04ebccb046060aeda4ed548cf186fafebd9
Александр <134267304+Alexanderrr123193@users.noreply.github.com>
2024-10-07 22:34:47 +0300

    Update Main.java

--- src/main/java/ru/a/reshetnikov/Main.java
+++ src/main/java/ru/a/reshetnikov/Main.java
@@ -2,6 +2,6 @@ package ru.a.reshetnikov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world-new-branch");
+        System.out.println("Hello world-new-branch22222222222222222222222");
     }
 }
